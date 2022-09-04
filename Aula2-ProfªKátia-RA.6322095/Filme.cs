599e72de402a274bf1c8f13c7607ce44f6ec9215
Math2 <Math2@DESKTOP-HD5Q5DJ>
2022-09-03 23:23:35 -0300

    Adicionar arquivos de projeto.

---
+++ Aula2-ProfªKátia-RA.6322095/Filme.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2_ProfªKátia_RA._6322095
+{
+    internal class Filme
+    {
+        string titulo;
+        string sinopse;
+        string genero;
+        string clasificacao;
+
+        public void Receber(string ti, string si, string ge, string cl)
+        {
+            this.titulo = ti;
+            this.sinopse = si;
+            this.genero = ge;
+            this.clasificacao = cl;
+
+        }
+        public string Mostrar()
+        {
+            return "Titulo = " + this.titulo +
+                "\nSinopse = " + this.sinopse +
+                "\nGenero = " + this.genero +
+                "\nclasificacao = " + this.clasificacao;
+        }
+
+    }
+}
+    
+
---
+++ Aula2-ProfªKátia-RA.6322095/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2_ProfªKátia_RA._6322095
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Filme Filme = new Filme();
+            Entrada entrada = new Entrada();
+
+            Console.WriteLine("Digite o Titulo: ");
+            string titulo = Console.ReadLine();
+
+            Console.WriteLine("Digite a Sinopse: ");
+            string sinopse = Console.ReadLine();
+
+            Console.WriteLine("Digite o Gênero: ");
+            string genero = Console.ReadLine();
+
+            Console.WriteLine("Digite a Classificação: ");
+            string clasificacao = (Console.ReadLine());
+
+            Filme.Receber(titulo, sinopse, genero, clasificacao);
+
+            Console.WriteLine("Digite o Valor: ");
+            entrada.SetValor(Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("Digite a Poltrona: ");
+            entrada.SetPoltrona(Console.ReadLine());
+
+            Console.WriteLine("Digite a Data: ");
+            entrada.SetData(Console.ReadLine());
+
+            Console.WriteLine("Digite a Hora: ");
+            entrada.SetHora(Console.ReadLine());
+
+            Console.Clear();
+
+
+            Console.WriteLine(Filme.Mostrar() + "\n\n" + "Valor: " + entrada.GetValor().ToString("C") + " Reais " +
+            "\nPoltrona: " + entrada.GetPoltrona() + "\nData: " + entrada.GetData() + "\nHora: " + entrada.GetHora());
+            Console.ReadKey();
+        }
+    }
+}
+        
